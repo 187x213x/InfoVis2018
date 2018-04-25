3b26e89199e47ddd47790104f50c5a5713ce077d
nakatani <shota-n@ws.cs.kobe-u.ac.jp>
2018-04-25 21:58:14 +0900

    add task3

---
+++ W06/task3.js
@@ -0,0 +1,70 @@
+function main()
+{
+    var width = 500;
+    var height = 500;
+
+    var scene1 = new THREE.Scene();
+    var scene2 = new THREE.Scene();
+
+    var fov = 45;
+    var aspect = width / height;
+    var near = 1;
+    var far = 1000;
+    var camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
+    camera.position.set( 0, 0, 5 );
+    scene1.add( camera );
+    scene2.add( camera );
+
+    var light = new THREE.PointLight();
+    light.position.set( 5, 5, 5 );
+    scene1.add( light );
+    scene2.add( light );
+
+    var renderer_gouraud = new THREE.WebGLRenderer();
+    renderer_gouraud.setSize( width, height );
+    document.body.appendChild( renderer_gouraud.domElement );
+
+    var renderer_phong = new THREE.WebGLRenderer();
+    renderer_phong.setSize( width, height );
+    document.body.appendChild( renderer_phong.domElement );
+
+    var geometry = new THREE.TorusKnotGeometry( 1, 0.3, 100, 20 );
+    var material_gouraud = new THREE.ShaderMaterial({
+        vertexColors: THREE.VertexColors,
+        vertexShader: document.getElementById('gouraud.vert').text,
+        fragmentShader: document.getElementById('gouraud.frag').text,
+        uniforms: {
+          light_position: { type: 'v3', value: light.position },
+          camera_position: { type: 'v3', value: camera.position }
+        }
+    });
+
+    var material_phong = new THREE.ShaderMaterial({
+        vertexColors: THREE.VertexColors,
+        vertexShader: document.getElementById('phong.vert').text,
+        fragmentShader: document.getElementById('phong.frag').text,
+        uniforms: {
+          light_position: { type: 'v3', value: light.position },
+          camera_position: { type: 'v3', value: camera.position }
+        }
+    });
+
+    var torus_knot_gouraud = new THREE.Mesh( geometry, material_gouraud );
+    scene1.add( torus_knot_gouraud );
+
+    var torus_knot_phong = new THREE.Mesh( geometry, material_phong );
+    scene2.add( torus_knot_phong );
+
+    loop();
+
+    function loop()
+    {
+        requestAnimationFrame( loop );
+        torus_knot_gouraud.rotation.x += 0.01;
+        torus_knot_gouraud.rotation.y += 0.01;
+        torus_knot_phong.rotation.x += 0.01;
+        torus_knot_phong.rotation.y += 0.01;
+        renderer_gouraud.render( scene1, camera );
+        renderer_phong.render( scene2, camera );
+    }
+}
